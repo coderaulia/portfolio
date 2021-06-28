f1a4e012be3af5c6ceb0287a1af121934b9df5f2
coderaulia <auliasw89@gmail.com>
2021-06-28 12:06:56 +0700

    homepage editing & break

--- system/home.php
+++ system/home.php
@@ -5,5 +5,27 @@
       <p>I design and build beautiful websites for social media influencers & businesses around the globe. If you need a modern, SEO friendly and powerful website, send me an email. If we are a good fit, I will give you a time and cost estimate.</p>
       <a href="portfolio.html" class="button primary">See My Work</a>
     </header>
+    <section class="custom-section">
+      <div class="row">
+        <div class="col-6 col-12-medium">
+          <h2>My Latest Work</h2>
+        </div>
+      </div>
+    </section>
+    <section class="custom-section">
+      <div class="row">
+        <div class="col-5 col-12-medium">
+          <h2>What I do?</h2>
+        </div>
+        <div class="col-7 col-12-medium">
+          <h3>Design</h3>
+          <p>I design beautiful and powerful websites for modern businesses. Any business today needs a website that wins customers’ trust and helps you do your business well. I make sure your website is up to that standard.</p>
+          <h3>Development</h3>
+          <p>I build websites in Wordpress, Webflow, or only HTML+CSS where I can create responsive, powerful and fully custom websites. Plus, Wordpress & Webflow has an incredibly simple Content Editor for you and your team to edit website content quickly and easily.</p>
+          <h3>Digital Marketing</h3>
+          <p>I also create digital marketing campaigns and strategies to increase engagement on your site and social media.</p>
+        </div>
+      </div>
+    </section>
   </div>
 </div>
