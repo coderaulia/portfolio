dfd97aec5255161aa7012a31c6314d135f0a81ff
coderaulia <auliasw89@gmail.com>
2021-06-28 15:36:38 +0700

    adding links feature

---
+++ system/links.php
@@ -0,0 +1,10 @@
+<main id="main">
+  <div class="inner">
+    <h2 class="text-center">Links</h2>
+    <a href="portfolio.html" class="button fit icon solid fa-tv">Portfolio</a>
+    <a href="https://instagram.com/coderaulia" class="button fit icon brands solid style1 fa-instagram">Instagram</a>
+    <a href="https://dribbble.com/coderaulia" class="button fit icon brands solid style1 fa-dribbble">Dribbble</a>
+    <a href="https://github.com/coderaulia" class="button fit icon brands solid style1 fa-github">Github</a>
+    <a href="https://www.youtube.com/channel/UCnIp59QJ1yY3Gorb7NANmTQ" class="button fit icon brands solid style1 fa-youtube">YouTube</a>
+  </div>
+</main>
