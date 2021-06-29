d290c231711b8e2dadc5c2732dfa34dfbffa8a89
coderaulia <auliasw89@gmail.com>
2021-06-29 11:21:39 +0700

    editing portfolio page

--- system/portfolio.php
+++ system/portfolio.php
@@ -11,9 +11,21 @@
       <hr>
       <div class="box alt">
         <div class="row">
-          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
-          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="" /></span></div>
-          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/langgeng.jpg" alt="Langgeng Sejahtera Indonesia" /></span>
+            <p><strong>PT Langgeng Sejahtera Indonesia</strong> company profile site - <a href="https://langgengsejahtera.com/" target="_blank">Live Site</a></p>
+          </div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="Kantor Hukum Roy dan Rekan" /></span>
+            <p><strong>Kantor Hukum Roy & Rekan</strong> company profile site - <a href="https://roydanrekan.com/" target="_blank">Live Site</a></p>
+          </div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/portfolio.jpg" alt="Coderaulia" /></span>
+            <p><strong>Coderaulia</strong> company profile site - <a href="https://pro.auliasatriow.com/" target="_blank">Live Site</a></p>
+          </div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/rumahpsi.jpg" alt="Rumah Psikologi Mahanani" /></span>
+            <p><strong>Rumah Psikologi Mahanani</strong> company profile site - <a href="https://rumahpsikologimahanani.com/" target="_blank">Live Site</a></p>
+          </div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/orgindo.jpg" alt="PT Orgindo" /></span>
+            <p><strong>PT Organik Inti Indonesia</strong> company profile site - <a href="https://orgindo.co.id/" target="_blank">Live Site</a></p>
+          </div>
         </div>
       </div>
     </section>
@@ -23,9 +35,9 @@
       <hr>
       <div class="box alt">
         <div class="row gtr-uniform">
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/kavlingbamboo.jpg" alt="Kavling Bamboo Garden Village" /></span>
+            <p><strong>Kavling Bamboo Garden Village</strong> landing page - <a href="https://kavlingbamboogarden.web.id/" target="_blank">Live Site</a></p>
+          </div>
         </div>
       </div>
     </section>
@@ -35,9 +47,10 @@
       <hr>
       <div class="box alt">
         <div class="row gtr-uniform">
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
+
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/matapena.jpg" alt="Mata Pena News" /></span>
+            <p><strong>Mata Pena News</strong> website - <a href="https://matapenanews.com/" target="_blank">Live Site</a></p>
+          </div>
         </div>
       </div>
     </section>
@@ -47,9 +60,10 @@
       <hr>
       <div class="box alt">
         <div class="row gtr-uniform">
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
+
+          <div class="col-4 col-12-medium">
+            <h3>Coming soon</h3>
+          </div>
         </div>
       </div>
     </section>
@@ -59,8 +73,12 @@
       <hr>
       <div class="box alt">
         <div class="row gtr-uniform">
-          <div class="col-4"><span class="image fit"><img src="assets/images/matapena.jpg" alt="" /></span></div>
-          <div class="col-4"><span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="" /></span></div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/assessment.jpg" alt="Assessment App" /></span>
+            <p><strong>Assessment App</strong>- <a href="https://app.rumahpsikologimahanani.com/" target="_blank">Live Site</a></p>
+          </div>
+          <div class="col-4 col-12-medium"><span class="image fit"><img src="assets/images/dosen-app.jpg" alt="Direktori Publikasi Dosen App" /></span>
+            <p><strong>Direktori Publikasi Dosen</strong> -Web-App - <a href="https://psikologi.unnes.ac.id/dosen" target="_blank">Live Site</a></p>
+          </div>
         </div>
       </div>
     </section>
