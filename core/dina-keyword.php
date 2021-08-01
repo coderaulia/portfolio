1ae894e5c5cee85a655f79bef8752a24ab68db15
coderaulia <auliasw89@gmail.com>
2021-08-01 22:31:10 +0700

    small fixes

--- core/dina-keyword.php
+++ core/dina-keyword.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_GET['mod'] == "home") {
-  echo "jasa pembuatan website, jasa web design, web design agency, wordpress design, wordpress design agency, coderaulia";
+  echo "jasa pembuatan website, jasa web design, web design agency, wordpress design, wordpress design agency, coderaulia, aulia satrio wibowo";
 } else {
-  echo "jasa pembuatan website, jasa web design, web design agency, wordpress design, wordpress design agency, coderaulia";
+  echo "jasa pembuatan website, jasa web design, web design agency, wordpress design, wordpress design agency, coderaulia, aulia satrio wibowo";
 }
--- system/contact.php
+++ system/contact.php
@@ -2,7 +2,7 @@
   <div class="inner">
     <header>
       <h1>Contact Me!</h1>
-      <p>If you need a elegant and powerful website for your business, startup or personally, I am available for work. Contact me as soon as possible.</p>
+      <p>If you need a elegant and powerful website for your business, startup or personally, I am available for work. Click on the button below, or you can reach me via my Social Media (Instagram, Twitter, or Whatsapp).</p>
       <a href="mailto:hi@auliasatriow.com" class="button primary">Email Me</a> <a href="portfolio.html" class="button">See My Work</a>
     </header>
   </div>
