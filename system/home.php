bb1f7cd83b3eb564afc1a1f569639500399d3da3
coderaulia <auliasw89@gmail.com>
2021-08-01 23:11:11 +0700

    update

--- system/home.php
+++ system/home.php
@@ -1,62 +1,95 @@
 <main id="main">
-  <div class="inner">
-    <header>
-      <h1>Hi! I'm Coderaulia, Web Designer <br />and Wordpress Developer.</h1>
-      <p>I design and build beautiful websites for individuals, community & businesses around the globe. If you need an elegant, SEO friendly and powerful website, send me an email. If we are a good fit, I will give you a time and cost estimate.</p>
-      <a href="portfolio.html" class="button primary">See My Work</a> <a href="mailto:hi@auliasatriow.com" class="button">Free Consultation</a>
-    </header>
-    <section class="custom-section">
-      <div class="row">
-        <div class="col-6 col-12-medium">
-          <h2 class="subtitle">My Latest Work</h2>
-          <h4 class="works">News Website</h4>
-          <p>A full website design & development for a news media company. I have build this website with Wordpress Extra Theme</p>
-          <a href="https://matapenanews.com" class="button small">See this project</a>
-        </div>
-        <div class="col-6 col-12-medium">
-          <span class="image fit"><img src="assets/images/matapena.jpg" alt="Mata Pena News - Coderaulia" /></span>
-        </div>
-      </div>
-    </section>
-    <section class="custom-section">
-      <div class="row">
-        <div class="col-6 col-12-medium">
-          <h2 class="subtitle">My Latest Work</h2>
-          <h4 class="works">Lawyer Website</h4>
-          <p>A full website design & development for a law company website. I have build this website with Divi theme Wordpress</p>
-          <a href="https://roydanrekan.com" class="button small">See this project</a>
-        </div>
-        <div class="col-6 col-12-medium">
-          <span class="image fit"><img src="assets/images/roydanrekan.jpg" alt="Roy dan Rekan - Coderaulia" /></span>
-        </div>
-      </div>
-    </section>
-    <section class="custom-section">
-      <div class="row">
-        <div class="col-5 col-12-medium">
-          <h2 class="works">What I do?</h2>
-        </div>
-        <div class="col-7 col-12-medium">
-          <h3>Design</h3>
-          <p>I design beautiful and powerful websites for <a href="business.html">modern businesses</a>. Any business today needs a website that wins customers’ trust and helps you do your business well. I make sure your website is up to that standard.</p>
-          <h3>Development</h3>
-          <p>I build websites in Wordpress, Webflow, or only HTML+CSS where I can create responsive, powerful and fully custom websites. Plus, Wordpress & Webflow has an incredibly simple Content Management System (CMS) for you and your team to edit website content quickly and easily.</p>
-          <h3>Digital Marketing</h3>
-          <p>I also create digital marketing campaigns and strategies to increase engagement on your site and social media.</p>
-        </div>
-      </div>
-    </section>
-    <section class="custom-section">
-      <div class="row">
-        <div class="col-5 col-12-medium">
-          <h2 class="works">Want to work?</h2>
-        </div>
-        <div class="col-7 col-12-medium">
-          <h3>If you need...</h3>
-          <p>If you need a modern and powerful website for your business, startup or personally, I am available for work. You can email me directly at <a href="mailto:hi@auliasatriow.com">hi@auliasatriow.com</a>.</p>
-          <a href="mailto:hi@auliasatriow.com" class="button">Free Consultation</a>
-        </div>
-      </div>
-    </section>
-  </div>
+   <div class="inner">
+      <header>
+         <h1>Hi! I'm Coderaulia, Web Designer <br />and Wordpress Developer.</h1>
+         <p>I design and build beautiful websites for individuals, community & businesses around the globe. If you need
+            an elegant, SEO friendly and powerful website, send me an email. If we are a good fit, I will give you a
+            time and cost estimate.</p>
+         <a href="portfolio.html" class="button primary">See My Work</a> <a href="mailto:hi@auliasatriow.com"
+            class="button">Free Consultation</a>
+      </header>
+      <section class="custom-section">
+         <div class="row">
+            <div class="col-6 col-12-medium">
+               <h1>About me...</h1>
+               <p>My name is Aulia, a fulltime Web Developer from Bogor, Indonesia. I have more than 5 years experiences in web development. And done some cool projects (you can find on <a href="portfolio.html" target="_blank">my portfolio </a>page).</p>
+            </div>
+            <div class="col-6 col-12-medium">
+               <p>I am comfortable to using several technologies, including:</p>
+               <ul class="icons">
+                  <li><a href="https://developer.mozilla.org/id/docs/Web/HTML" class="icon brands style2 fa-html5"><span class="label">HTML</span></a></li>
+                  <li><a href="https://www.w3.org/Style/CSS/Overview.en.html" class="icon brands style2 fa-css3-alt"><span class="label">CSS</span></a></li>
+                  <li><a href="https://getbootstrap.com" class="icon brands style2 fa-bootstrap"><span class="label">Bootstrap</span></a></li>
+                  <li><a href="https://php.net" class="icon brands style2 fa-php"><span class="label">PHP</span></a></li>
+                  <li><a href="https://www.javascript.com/" class="icon brands style2 fa-js"><span class="label">Javascript</span></a></li>
+                  <li><a href="https://github.com/coderaulia" class="icon brands style2 fa-git-alt"><span class="label">Git</span></a></li>
+                  <li><a href="https://wordpress.org" class="icon brands style2 fa-wordpress"><span class="label">Wordpress</span></a></li>
+                  <li><a href="https://elementor.com" class="icon brands style2 fa-elementor"><span class="label">Elementor</span></a></li>
+                  <li><a href="https://figma.com" class="icon brands style2 fa-figma"><span class="label">Figma</span></a></li>
+               </ul>
+            </div>
+         </div>
+      </section>
+      <section class="custom-section">
+         <div class="row">
+            <div class="col-6 col-12-medium">
+               <h2 class="subtitle">My Latest Work</h2>
+               <h4 class="works">Landing Pages</h4>
+               <p>A landing pages for a Property Developer company in Bogor. I made this website with Wordpress and Elementor</p>
+               <a href="https://theortensiavillage.com" class="button small">See this project</a>
+            </div>
+            <div class="col-6 col-12-medium">
+               <span class="image fit"><img src="assets/images/theortensiavillage.jpg" alt="The Ortensia Village - Coderaulia" /></span>
+            </div>
+         </div>
+      </section>
+      <section class="custom-section">
+         <div class="row">
+            <div class="col-6 col-12-medium">
+               <h2 class="subtitle">My Latest Work</h2>
+               <h4 class="works">Lawyer Website</h4>
+               <p>A full website design & development for a law company website. I have build this website with Divi
+                  theme Wordpress.</p>
+               <a href="https://roydanrekan.com" class="button small">See this project</a>
+            </div>
+            <div class="col-6 col-12-medium">
+               <span class="image fit"><img src="assets/images/roydanrekan.jpg"
+                     alt="Roy dan Rekan - Coderaulia" /></span>
+            </div>
+         </div>
+      </section>
+      <section class="custom-section">
+         <div class="row">
+            <div class="col-5 col-12-medium">
+               <h2 class="works">What I do?</h2>
+            </div>
+            <div class="col-7 col-12-medium">
+               <h3>Design</h3>
+               <p>I design beautiful and powerful websites for <a href="business.html">modern businesses</a>. Any
+                  business today needs a website that wins customers’ trust and helps you do your business well. I make
+                  sure your website is up to that standard.</p>
+               <h3>Development</h3>
+               <p>I build websites in Wordpress, Webflow, or only HTML+CSS where I can create responsive, powerful and
+                  fully custom websites. Plus, Wordpress & Webflow has an incredibly simple Content Management System
+                  (CMS) for you and your team to edit website content quickly and easily.</p>
+               <h3>Digital Marketing</h3>
+               <p>I also create digital marketing campaigns and strategies to increase engagement on your site and
+                  social media.</p>
+            </div>
+         </div>
+      </section>
+      <section class="custom-section">
+         <div class="row">
+            <div class="col-5 col-12-medium">
+               <h2 class="works">Want to work?</h2>
+            </div>
+            <div class="col-7 col-12-medium">
+               <h3>If you need...</h3>
+               <p>If you need a modern and powerful website for your business, startup or personally, I am available for
+                  work. You can email me directly at <a href="mailto:hi@auliasatriow.com">hi@auliasatriow.com</a>.</p>
+               <a href="mailto:hi@auliasatriow.com" class="button">Free Consultation</a>
+            </div>
+         </div>
+      </section>
+   </div>
 </main>
